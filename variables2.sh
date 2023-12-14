f0d4436fd1ba2b82a01d9e3e3cc505af7fa84dcf
manojreddy <manojmulukuri@outlook.com>
2023-12-14 08:51:01 +0530

    my third script

---
+++ variables2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+X=$1  #here $1 and $2 acts as arguments that gives the option of entering the names to the user
+Y=$2  #without disturbing the script written during execution
+echo "$X : HOW ARE YOU, $Y?"
+echo "$Y : im good, how about you, $X?"
