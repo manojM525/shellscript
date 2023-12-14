363735ed76aa1be36ae218c9d05fbc46fc6edcf8
manojreddy <manojmulukuri@outlook.com>
2023-12-14 14:54:07 +0530

    MODIFIED LOGINCREDENTIALS

--- logincredentials3.sh
+++ logincredentials3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -sp "enter username: " $USERNAME
-read -sp "enter password: " $PASSWORD
+read -sp "enter username: " USERNAME
+read -sp "enter password: " PASSWORD
 echo "YOUR USERNAME IS $USERNAME AND YOUR PASSWORD IS $PASSWORD"
