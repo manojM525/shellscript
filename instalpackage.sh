c647e3073034c857a3092f4ba62e090848fd178b
manojreddy <manojmulukuri@outlook.com>
2023-12-15 13:26:00 +0530

    installing packages first edition

---
+++ instalpackage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ID=$(id -u)
+if $ID -ne 0    #checking whether the person running the script is a root user or not as packages can only be installed by root users.
+echo "you are not a root user"
+exit 1
+else
+echo "you are a root user"
+fi
+dnf install git -y   #installing git package.
+#now there is a need to check whether it got installed properly or not.
+#the success or failure of previous command is generally stored in $?
+if $? -ne 0 
+echo "installing git is ...FAILED"
+exit 1
+else
+echo "installing git is ...SUCCESS"
+fi
